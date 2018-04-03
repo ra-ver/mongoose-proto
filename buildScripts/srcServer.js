dd21bc5513acb85c80c2fcbd371785b5efbdf571
Rahul Verma <Rahul.Verma@oicr.on.ca>
2018-04-03 13:40:41 -0400

    Base API Design

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -9,23 +9,18 @@ import config from '../webpack.config.dev';
 const port = 3000;
 const app = express();
 const compiler = webpack(config);
+// Setup as dev application
+process.env.NODE_ENV = 'DEV';
 
-app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: true,
-  publicPath: config.output.publicPath
-}));
+app.use(
+  require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath,
+  })
+);
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
-});
-
-app.get('/users', function(req, res) {
-  // Hard coding for simplicity. Pretend this hits a real database
-  res.json([
-    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
-    {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
-    {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
-  ]);
 });
 
 app.listen(port, function(err) {
